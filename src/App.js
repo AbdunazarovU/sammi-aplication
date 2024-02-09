9cb5e9a0bbe6492630d949c013f7fb2d755591cd
AbdunazarovU <abdunazarov.ulugbek63@gmail.com>
2024-02-09 16:54:43 +0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Routes, Route } from "react-router-dom"
+import { Login, Main, Register } from "./components"
+
 const App = () => {
   return (
     <div>
-      App
+      <Routes>
+        <Route path="/" element={<Main/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+      </Routes>
     </div>
   )
 }
