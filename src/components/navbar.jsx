73fed861ca02a85bfe337989914521f4ee030d43
AbdunazarovU <abdunazarov.ulugbek63@gmail.com>
2024-02-09 17:29:50 +0500

    remove trash script

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom"
-import { logo1 } from "../constants"
+import { logo } from "../constants"
 
 const Navbar = () => {
   return (
     <div class="container d-flex flex-column flex-md-row align-items-center pt-3 pb-3 mb-4 border-bottom">
       <Link to={"/"}>
-        <img width={"200px"} height={"60px"} style={{objectFit: "cover"}} src={logo1} alt="site logo" />
+        <img width={"200px"} height={"60px"} style={{objectFit: "cover"}} src={logo} alt="site logo" />
       </Link>
 
       <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,1 @@
-export const logo = "https://cdn.freebiesupply.com/logos/large/2x/unicon-1-logo-png-transparent.png"
-export const logo1 = "https://cdn.worldvectorlogo.com/logos/unicon-1.svg"
+export const logo = "https://cdn.worldvectorlogo.com/logos/unicon-1.svg"
