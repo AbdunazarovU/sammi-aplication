80ca04dbded1e23e3828a393ef4421b4d4a5b93b
AbdunazarovU <abdunazarov.ulugbek63@gmail.com>
2024-02-09 18:29:58 +0500

    add login and register components

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,7 +1,26 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import { useState } from "react";
+import { Input } from "../ui";
 
-export default Login
+const Login = () => {
+  const [email, setEmail] = useState("")
+  const [pass, setPass] = useState("")
+
+  return (
+    <div className="text-center w-25 mx-auto mt-5">
+      <main class="form-signin w-100 m-auto">
+        <form>
+          <h1 class="h3 mb-3 fw-normal">Please login</h1>
+
+          <Input type="email" label={"Email addres"} state={email} setState={setEmail} />
+          <Input type="password" label={"Password"} state={pass} setState={setPass} />
+
+          <button class="btn btn-primary w-100 py-2 mt-2" type="submit">
+            Login
+          </button>
+        </form>
+      </main>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,7 +1,28 @@
-const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+import { useState } from "react";
+import { Input } from "../ui";
 
-export default Register
+const Register = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [pass, setPass] = useState("")
+
+  return (
+    <div className="text-center w-25 mx-auto mt-5">
+      <main class="form-signin w-100 m-auto">
+        <form>
+          <h1 class="h3 mb-3 fw-normal">Please register</h1>
+
+          <Input label={"Usarname"} state={name} setState={setName} />
+          <Input type="email" label={"Email addres"} state={email} setState={setEmail} />
+          <Input type="password" label={"Password"} state={pass} setState={setPass} />
+
+          <button class="btn btn-primary w-100 py-2 mt-2" type="submit">
+            Register
+          </button>
+        </form>
+      </main>
+    </div>
+  );
+};
+
+export default Register;
